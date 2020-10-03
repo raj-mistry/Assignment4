c5d573416712c56092d6c6028b009c8d3dd4411a
raj-mistry <56315897+raj-mistry@users.noreply.github.com>
2020-10-03 12:05:03 -0400

    1

---
+++ app/src/main/java/com/example/assignment4/GalleryActivity.java
@@ -0,0 +1,67 @@
+package com.example.assignment4;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Created by User on 1/2/2018.
+ */
+
+public class GalleryActivity extends AppCompatActivity {
+
+    private static final String TAG = "GalleryActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+        Log.d(TAG, "onCreate: started.");
+
+        getIncomingIntent();
+    }
+
+    private void getIncomingIntent(){
+        Log.d(TAG, "getIncomingIntent: checking for incoming intents.");
+
+        if(getIntent().hasExtra("image_url") && getIntent().hasExtra("image_name")&& getIntent().hasExtra("item_detail")){
+            Log.d(TAG, "getIncomingIntent: found intent extras.");
+
+            String imageUrl = getIntent().getStringExtra("image_url");
+            String imageName = getIntent().getStringExtra("image_name");
+            String itemDetail = getIntent().getStringExtra("item_detail");
+
+            setImage(imageUrl, imageName,itemDetail);
+        }
+    }
+
+
+    private void setImage(String imageUrl, String imageName, String itemDetail){
+        Log.d(TAG, "setImage: setting te image and name to widgets.");
+
+        TextView name = findViewById(R.id.image_description);
+        name.setText(imageName);
+
+        TextView detail = findViewById(R.id.item_detail);
+        detail.setText(itemDetail);
+
+        ImageView image = findViewById(R.id.image);
+        Glide.with(this)
+                .asBitmap()
+                .load(imageUrl)
+                .into(image);
+
+    }
+
+}
+
+
+
+
--- app/src/main/java/com/example/assignment4/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/assignment4/RecyclerViewAdapter.java
@@ -1,4 +1,106 @@
 package com.example.assignment4;
 
-public class RecyclerViewAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by User on 1/1/2018.
+ */
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<String> mImageNames = new ArrayList<>();
+    private ArrayList<String> mImages = new ArrayList<>();
+    private ArrayList<String> mDescription = new ArrayList<>();
+    private ArrayList<String> mDetail = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(Context context, ArrayList<String> imageNames, ArrayList<String> images , ArrayList<String> descriptions, ArrayList<String> detaileddescriptions) {
+        mImageNames = imageNames;
+        mImages = images;
+        mDescription = descriptions;
+        mDetail = detaileddescriptions;
+        mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.listitem, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+
+        //Glide.with(mContext)
+        //        .asBitmap()
+        //        .load(mImages.get(position));
+        //.into(holder.image);
+
+        holder.imageName.setText(mImageNames.get(position));
+        holder.imageDescription.setText(mDescription.get(position));
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked on: " + mImageNames.get(position));
+
+                Toast.makeText(mContext, mImageNames.get(position), Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(mContext, GalleryActivity.class);
+                intent.putExtra("image_url", mImages.get(position));
+                intent.putExtra("image_name", mImageNames.get(position));
+                intent.putExtra("item_detail", mDetail.get(position));
+                mContext.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageNames.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        //CircleImageView image;
+        TextView imageName;
+        TextView imageDescription;
+        TextView imageDetail;
+        LinearLayout parentLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            //image = itemView.findViewById(R.id.image);
+            imageDetail = itemView.findViewById(R.id.item_detail);
+            imageName = itemView.findViewById(R.id.image_name);
+            imageDescription = itemView.findViewById(R.id.image_description);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
 }
